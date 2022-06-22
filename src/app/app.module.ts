57eae1e35694d003500d2125aac3e9dbbeee8368
AravindhRamasamy <raravindhashok@gmail.com>
2022-06-22 22:49:06 +0530

    patch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app/app-routing.module';
-import { AppComponent } from '../app/app.component';
-import { HeaderComponent } from '../app/header/header.component';
-import { FooterComponent } from '../app/footer/footer.component';
-import { HomeComponent } from '../app/home/home.component';
-import { ContactmeComponent } from '../app/contactme/contactme.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { FooterComponent } from 'src/app/footer/footer.component';
+import { HomeComponent } from 'src/app/home/home.component';
+import { ContactmeComponent } from 'src/app/contactme/contactme.component';
 
 @NgModule({
   declarations: [
