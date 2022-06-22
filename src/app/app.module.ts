09d53f443e7b88b73680a0860cf7898e78450493
AravindhRamasamy <raravindhashok@gmail.com>
2022-06-22 22:42:49 +0530

    package json update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ContactmeComponent } from './contactme/contactme.component';
+import { AppRoutingModule } from '../app/app-routing.module';
+import { AppComponent } from '../app/app.component';
+import { HeaderComponent } from '../app/header/header.component';
+import { FooterComponent } from '../app/footer/footer.component';
+import { HomeComponent } from '../app/home/home.component';
+import { ContactmeComponent } from '../app/contactme/contactme.component';
 
 @NgModule({
   declarations: [
